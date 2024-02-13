a7d94081821de042723bbd977dddf60e62e29116
Harry Raj <113687752+harry10-git@users.noreply.github.com>
2024-02-13 11:19:13 +0530

    Delete week5_/ques4 directory

--- week5_/ques4/static/app4.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function () {
-    var greeting = 'heaskfdnadjn';
-    $(".message-div").html(greeting);
-
-});
--- week5_/ques4/views.py
+++
@@ -1,9 +0,0 @@
-from django.shortcuts import render
-from django.http import HttpResponse
-
-
-# Create your views here.
-
-def display(request):
-    # return HttpResponse('ques 4')
-    return render(request,'index4.html')
